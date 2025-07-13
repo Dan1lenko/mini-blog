97dd144924f2935108d6738ec848a5fee31a642a
Dan1lenko <hdm070206@gmail.com>
2025-07-13 14:54:47 +0300

    4

--- mini-blog/lib/firebase.js
+++ mini-blog/lib/firebase.js
@@ -1,13 +1,12 @@
 // lib/firebase.js
 
-import config from "@/postcss.config.mjs";
 import { initializeApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // import { getAnalytics } from "firebase/analytics"; ❌ прибери
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY, // ✅ виправлено
+  apiKey: "AIzaSyA5LtnZRPJ12JEGhjA-Yaw2h9Zq3UVlmmk",
   authDomain: "mini-blog-a4ab3.firebaseapp.com",
   projectId: "mini-blog-a4ab3",
   storageBucket: "mini-blog-a4ab3.appspot.com", // ✅ виправлено
